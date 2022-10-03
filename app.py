01c8c4fd1b686e23a96d9ab847db6a893801db35
Samuel Mayers <samuel.mayers22@hotmail.com>
2022-10-02 21:30:13 -0400

    add SQLAlchemy

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-
+@app.route('/')
+def home():
+    return 'Hello'
 
 if __name__ == '__main__':
     app.run(debug=True)
