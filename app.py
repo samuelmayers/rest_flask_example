e4cce7f72b7d6f0fa0fe78cb57a50c72bbb8adea
Samuel Mayers <samuel.mayers22@hotmail.com>
2022-10-02 22:48:05 -0400

    html

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello'
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
