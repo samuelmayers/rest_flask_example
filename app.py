2e5b1c9fca0d937e4743538ed63465607086b20c
Samuel Mayers <samuel.mayers22@hotmail.com>
2022-10-04 18:54:03 -0400

    all flask route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,6 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/add')
+def add():
+    return redirect(url_for('home'))
+
+
+@app.route('/edit')
+def edit():
+    return redirect(url_for('home'))
+
+@app.route('/delete')
+def delete():
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
